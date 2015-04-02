6ce5f0e2726696bf47d5bdf2c90bd74569554726
laixintao <799303812@163.com>
2015-04-02 12:31:05 +0800

    Move my project to Xcode.
    I use c++ to write the compiler,but can’t use VS on my mac,so I decide
    to use Xcode to continue.Now I move it to Xcode.

---
+++ PointerPascal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PointerPascal
+//
+//  Created by Sinpeach on 15/4/2.
+//  Copyright (c) 2015年 laixintao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
